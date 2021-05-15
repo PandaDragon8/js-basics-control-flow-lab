f2b2e73c67d541bb0e37ccdf961da49277fdcb6d
Min <mtan121@aucklanduni.ac.nz>
2021-05-15 14:28:53 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!"
+  } 
+  
+  if (distance > 2500){
+  return "No can do."
+  }
+
+  if (distance > 2000) {
+
+    return "I will gladly take your thirty bucks."
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  return City==="NYC"? "Ok, sounds good.":"No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip ==="generous") {
+return "Thank you so much."
 }
+
+else if (tip==="not as generous"){
+return "Thank you."
+
+}
+
+else if (tip="if anything else"){
+  return "Bye."
+  
+  }
+
+}
+
+
